53a26c1a2d3d453ff26baf9aba633c3d5793d2fc
Abdul-Kalam-B <abdulkalamb23@gmail.com>
2024-11-05 14:18:16 +0530

    Create Q_1.py

---
+++ Day1/Q_1.py
@@ -0,0 +1,15 @@
+file_path = "C:/Users/pc/Desktop/python practice/Test_1/TVAQueries.txt"
+
+counter = 0
+
+with open(file_path , 'r' , encoding='utf-8') as file1:
+
+    data = file1.readlines()
+
+    for line in data:
+        words = line.split()
+        first_word = set(words)
+        counter = len(first_word)
+
+print(f"The first non-repeating words {first_word} ")      
+print(f"The total count of words {counter} ")  
