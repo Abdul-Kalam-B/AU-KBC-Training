298beeda56589a78d843b3cfe563e707fb41d490
Abdul-Kalam-B <abdulkalamb23@gmail.com>
2024-11-05 14:22:03 +0530

    Create l1.py

---
+++ Day2/l1.py
@@ -0,0 +1,11 @@
+# Adding elements in list 
+numbers = [10, 20, 30, 40, 50]
+
+first_element = numbers[0]      
+last_element = numbers[-1]     
+
+middle_elements = numbers[1:4]  
+
+print("First element:", first_element)
+print("Last element:", last_element)
+print("Middle elements:", middle_elements)
